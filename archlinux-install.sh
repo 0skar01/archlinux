c39ea68e753e9c6c566bd2a59dd7fbc216b3b0b0
0skar01 <100316643+0skar01@users.noreply.github.com>
2022-02-27 05:19:57 +0100

    added the last comment

--- archlinux-install.sh
+++ archlinux-install.sh
@@ -18,10 +18,10 @@ passwd
 pacman -S --noconfirm grub efibootmgr networkmanager network-manager-applet base-devel dialog mtools dosfstools wpa_supplicant reflector linux-headers xdg-utils xdg-user-dirs rsync inetutils nfs-utils gvfs gvfs-smb openssh xf86-video-intel cups hplip pulseaudio alsa-utils pavucontrol terminus-font
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
-systenctl enable NetworkManager
-systenctl enable sshd
-systenctl enable reflector.timer
-systenctl enable cups
+systemctl enable NetworkManager
+systemctl enable sshd
+systemctl enable reflector.timer
+systemctl enable cups
 useradd -mG wheel $username
 passwd $username
 pacman -S sudo
@@ -30,6 +30,10 @@ sudo pacman -S --needed xfce4 mousepad parole ristretto thunar-archive-plugin th
 sudo pacman -S --needed xfce4-goodies
 sudo pacman -S --needed virtualbox-guest-utils xf86-video-vmware
 sudo systemctl enable lightdm
+echo "''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''"
+echo "FOLLOW THESE CONSTRUCTIONS:"
 echo "run visudo and uncomment line 82 (to line number enter :set number)"
 echo "then these orders in order: exit      umouunt -a      reboot"
 echo "go go go"
+echo "__________________________________________________________________________"
+echo ":P"
