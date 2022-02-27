cacf018631e872c2b6b3362922bee0b2c261916b
0skar01 <100316643+0skar01@users.noreply.github.com>
2022-02-27 04:22:17 +0100

    added grafische oberfläche

--- archlinux-install.sh
+++ archlinux-install.sh
@@ -25,3 +25,11 @@ systenctl enable cups
 useradd -mG wheel $username
 passwd $username
 pacman -S sudo
+sudo pacman -S --needed xorg
+sudo pacman -S --needed xfce4 mousepad parole ristretto thunar-archive-plugin thunar-media-tags-plugin xfce4-battery-plugin xfce4-datetime-plugin xfce4-mount-plugin xfce4-netload-plugin xfce4-notifyd xfce4-pulseaudio-plugin xfce4-screensaver xfce4-taskmanager xfce4-wavelan-plugin xfce4-weather-plugin xfce4-whiskermenu-plugin xfce4-xkb-plugin file-roller network-manager-applet leafpad epdfview galculator lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings capitaine-cursors arc-gtk-theme xdg-user-dirs-gtk
+sudo pacman -S --needed xfce4-goodies
+sudo pacman -S --needed virtualbox-guest-utils xf86-video-vmware
+sudo systemctl enable lightdm
+echo "run visudo and uncomment line 82 (to line number enter :set number)"
+echo "then these orders in order: exit      umouunt -a      reboot"
+echo "go go go"
