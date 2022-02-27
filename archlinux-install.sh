96fe8bcf61816f20d1472ca315b02267b54c7c2e
0skar01 <100316643+0skar01@users.noreply.github.com>
2022-02-27 03:44:30 +0100

    deleted parathesis form program

--- archlinux-install.sh
+++ archlinux-install.sh
@@ -15,7 +15,7 @@ echo "127.0.0.1     localhost" >> /etc/hosts
 echo "::1           localhost" >> /etc/hosts
 echo "127.0.1.1     $hostname.localdomain   $hostname" >> /etc/hosts
 passwd
-pacman -S --noconfirm grub efibootmgr networkmanager network-manager-applet base-devel dialog mtools dosfstools wpa_supplicant reflector linux-headers xdg-utils xdg-user-dirs rsync inetutils nfs-utils gvfs gvfs-smb openssh xf86-video-intel (bei intel graphic karte) xf86-video-andgpu (bei amd) nvidia nvidia-utils (bei nvidia graphic karte) bluez bluez-utils (für Bluetooth) cups hplip (für drücker) pulseaudio alsa-utils pavucontrol terminus-font
+pacman -S --noconfirm grub efibootmgr networkmanager network-manager-applet base-devel dialog mtools dosfstools wpa_supplicant reflector linux-headers xdg-utils xdg-user-dirs rsync inetutils nfs-utils gvfs gvfs-smb openssh xf86-video-intel cups hplip pulseaudio alsa-utils pavucontrol terminus-font
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 systenctl enable NetworkManager
