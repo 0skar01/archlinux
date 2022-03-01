4f0562e229ceea40e2c63d01796b24f3cceeb90f
0skar01 <100316643+0skar01@users.noreply.github.com>
2022-03-01 14:36:41 +0100

    UTF corrected

--- archlinux-install.sh
+++ archlinux-install.sh
@@ -15,7 +15,7 @@ hwclock --systohc
 echo "+++++++++++++++++++++++++++++++++++++++++++"
 echo "Ihre Sprache und Tastatur auf Deutsch einstellen"
 echo "+++++++++++++++++++++++++++++++++++++++++++"
-echo "de-DE.UTF-8" >> /etc/locale.gen
+echo "de_DE.UTF-8 UTF-8" >> /etc/locale.gen
 echo "LANG=de_DE.UTF-8" >> /etc/locale.conf
 locale-gen
 echo "KEYMAP=de-latin1" >> /etc/vconsole.conf
