8af93aa49b7da05051b0e3ad47a3fdb6528de008
0skar01 <100316643+0skar01@users.noreply.github.com>
2022-03-01 14:12:35 +0100

    added --noconfirm to intallation commands

--- archlinux-install.sh
+++ archlinux-install.sh
@@ -66,10 +66,10 @@ echo "++++++++++++++++++++++++++++++++++++++++++++++++        +++++++"
 read answer
 if [ $answer == 'J' ] || [ $answer == 'j' ]
 then
-    sudo pacman -S --needed xorg
-    sudo pacman -S --needed xfce4 mousepad parole ristretto thunar-archive-plugin thunar-media-tags-plugin xfce4-battery-plugin xfce4-datetime-plugin xfce4-mount-plugin xfce4-netload-plugin xfce4-notifyd xfce4-pulseaudio-plugin xfce4-screensaver xfce4-taskmanager xfce4-wavelan-plugin xfce4-weather-plugin xfce4-whiskermenu-plugin xfce4-xkb-plugin file-roller network-manager-applet leafpad epdfview galculator lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings capitaine-cursors arc-gtk-theme xdg-user-dirs-gtk
-    sudo pacman -S --needed xfce4-goodies
-    sudo pacman -S --needed virtualbox-guest-utils xf86-video-vmware
+    sudo pacman -S --noconfirm --needed xorg
+    sudo pacman -S --noconfirm --needed xfce4 mousepad parole ristretto thunar-archive-plugin thunar-media-tags-plugin xfce4-battery-plugin xfce4-datetime-plugin xfce4-mount-plugin xfce4-netload-plugin xfce4-notifyd xfce4-pulseaudio-plugin xfce4-screensaver xfce4-taskmanager xfce4-wavelan-plugin xfce4-weather-plugin xfce4-whiskermenu-plugin xfce4-xkb-plugin file-roller network-manager-applet leafpad epdfview galculator lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings capitaine-cursors arc-gtk-theme xdg-user-dirs-gtk
+    sudo pacman -S --noconfirm --needed xfce4-goodies
+    sudo pacman -S --noconfirm --needed virtualbox-guest-utils xf86-video-vmware
     sudo systemctl enable lightdm
 fi
 echo "--------------------------------------------------------------------------"
